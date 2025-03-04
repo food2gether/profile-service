576c55fc91897699db31e06c79c456d9993a8062
Marfien <contact@marfien.dev>
2025-03-04 13:30:07 +0100

    chore: bump shared-model version

--- src/main/java/org/github/food2gether/profileservice/ExceptionMapper.java
+++ src/main/java/org/github/food2gether/profileservice/ExceptionMapper.java
@@ -1,11 +1,8 @@
 package org.github.food2gether.profileservice;
 
 import com.github.food2gether.shared.ExceptionHandler;
-import io.quarkus.logging.Log;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
 import jakarta.ws.rs.core.Response;
-import org.jboss.logging.Logger;
 import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
 
 @ApplicationScoped
